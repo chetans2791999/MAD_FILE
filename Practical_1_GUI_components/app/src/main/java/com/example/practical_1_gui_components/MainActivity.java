fd7ca9c9e480f04acd32d70184108a99cc57002f
chetans2791999 <65163233+chetans2791999@users.noreply.github.com>
2021-06-02 03:42:54 +0530

    Add files via upload

---
+++ Practical_1_GUI_components/app/src/main/java/com/example/practical_1_gui_components/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gui_components;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.practical_1_gui_components.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
